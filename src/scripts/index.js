b8bf8f90609bdeeea3980dfb9c020851321a56c9
“annisatahira” <annisatahira999@gmail.com>
2021-06-28 22:04:50 +0700

    add button, nav, drawer init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,44 +1,33 @@
-import "regenerator-runtime"; /* for async await transpile */
-import "../styles/main.css";
-import List from "../templates/components/container/list.js";
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import List from '../templates/components/container/list';
 
-import "../templates/components/items/about-item.js";
-import "../templates/components/items/featured-item.js";
-import "../templates/components/items/product-item.js";
+import '../templates/components/items/about-item';
+import '../templates/components/items/featured-item';
+import '../templates/components/items/product-item';
 
-import { aboutData, featuredData } from "../data/home.js";
-import data from "../data/DATA.json";
+import { aboutData, featuredData } from '../data/home';
+import data from '../data/DATA.json';
+import App from './views/app';
 
-const drawerMobileElement = document.querySelector("#hamburger");
-const drawerElement = document.querySelector("#drawer");
-const faToggleElement = document.querySelector("#fa-toggle");
-
-drawerMobileElement.addEventListener("click", (event) => {
-  drawerElement.classList.toggle("open");
-  // check if toggle is open
-  if (drawerElement.classList.contains("open")) {
-    faToggleElement.classList.remove("fa-bars");
-    faToggleElement.classList.add("fa-close");
-  } else {
-    faToggleElement.classList.remove("fa-close");
-    faToggleElement.classList.add("fa-bars");
-  }
-
-  event.stopPropagation();
+const app = new App({
+  button: document.querySelector('#hamburger'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#content'),
 });
 
 // Render about list item
-const aboutEL = document.querySelector(".about-list");
-const aboutList = new List(aboutEL, "about-item", aboutData);
+const aboutEL = document.querySelector('.about-list');
+const aboutList = new List(aboutEL, 'about-item', aboutData);
 aboutList.render();
 
 // Render Featured list item
-const featuredEl = document.querySelector(".featured-list");
-const featuredList = new List(featuredEl, "featured-item", featuredData);
+const featuredEl = document.querySelector('.featured-list');
+const featuredList = new List(featuredEl, 'featured-item', featuredData);
 featuredList.render();
 
 // Render Restaurant list item
 const productData = data.restaurants;
-const productEl = document.querySelector(".product-list");
-const productList = new List(productEl, "product-item", productData);
+const productEl = document.querySelector('.product-list');
+const productList = new List(productEl, 'product-item', productData);
 productList.render();
